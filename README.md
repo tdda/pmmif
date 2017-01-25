93ac9ee6ce0578f516a31b6331970d8d07f755cb
Simon Brown <simon@StochasticSolutions.com>
2017-01-25 11:06:58 +0000

    Restructured pmmif package to make it so it can be built by setuptools.

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# pmmif
 Predictive Modelling Metadata Interchange Format
+================================================
+
+Python support for dataset metadata for predictive modelling and data analysis.
+
+The `pmmif` package provides two levels of support.
+
+.pmm Metadata Files
+--------------------
+
+Classes for reading and writing extended metadata to .pmm JSON files.
+
+
+Pandas DataFrame Support
+------------------------
+
+Classes for associating Pandas DataFrames with additional `type` and
+`tags` annotation metadata, including support for persisting additional
+metadata alongside `feather` datafiles.
+
--- setup.py
+++ setup.py
@@ -1,17 +1,43 @@
+import os
 from setuptools import setup, find_packages
+
+from pmmif.version import version as __version__
+
+def read(fname):
+    # read contents of file
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+def data(path, pathitems):
+    # build list of additional files to package up from a subdirectory
+    names = []
+    for relpath in pathitems:
+        subpath = path + [relpath]
+        dirname = os.path.join(*subpath)
+        for name in os.listdir(dirname):
+            pathname = os.path.join(relpath, name)
+            fullpathname = os.path.join(dirname, name)
+            if os.path.isdir(fullpathname):
+                names.extend(data(path, [pathname]))
+            else:
+                names.append(pathname)
+    return names
+
 setup(
-    name = 'pmmif',
-    version = '@@VERSION@@',
-    author = 'Stochastic Solutions Limited',
-    description = 'Predictive Modelling Metadata Interchange Format',
-    license = 'MIT',
-    url = 'http://www.tdda.info',
-    keywords = 'predictive modelling metadata',
-    namespace_packages = ['pmmif'],
-    packages = find_packages(),
-    package_data = {
-        '': [ 'data/*.*', 'doc/Makefile', 'doc/source/*.*'],
+    name='pmmif',
+    version=__version__,
+    author='Stochastic Solutions Limited',
+    author_email='info@StochasticSolutions.com',
+    description='Predictive Modelling Metadata Interchange Format',
+    long_description=read('README.md'),
+    license='MIT',
+    url='http://www.tdda.info',
+    download_url='https://github.com/tdda/pmmif',
+    keywords='predictive modelling metadata',
+    packages=find_packages(),
+    package_data={
+        'pmmif': ['README.md', 'LICENSE.txt'] + data(['pmmif'],
+                                                     ['doc', 'data']),
     },
     zip_safe=False,
-    install_requires = ['numpy>=1.9'],
+    install_requires=['numpy>=1.9'],
 )
